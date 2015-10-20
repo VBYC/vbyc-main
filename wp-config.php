2a7992c86b872df1a64433ccfc9818b7b5a675a7
Nathan Hiemstra <nathanh@criticalmass.com>
2015-10-19 20:55:10 -0500

    Adding live site config info- then I'll ignore this file.

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 define('DB_NAME', 'dev_vanburenyouthcamp_or');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'devvanburenyouth');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'NWiBUxtq');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.dev.vanburenyouthcamp.org');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
