64935f1e20acf4bcd2fdb3df87359f3312d5c425
Nathan Hiemstra <nathanh@criticalmass.com>
2015-11-04 07:17:16 -0600

    Header responsive progress

--- wp-content/themes/vbyc-custom/_template-basic-with-side-nav.php
+++ wp-content/themes/vbyc-custom/_template-basic-with-side-nav.php
@@ -22,19 +22,23 @@
     <header class="header-main v-offset-bot-0">
         <div class="container">
             <div class="row">
-                <div class="main-logo-container col-xs-12 col-md-4 text-center">
+                <div class="main-logo-container col-xs-9 col-sm-12 col-md-4 text-center">
                     <a href="/" title="Home">
-                        <img src="images/header/vbyc-logo.png" alt="VBYC logo" class="main-logo img-responsive">
+                        <img src="images/header/vbyc-logo-icon.png" alt="VBYC logo" class="main-logo main-logo-icon">
+                        <img src="images/header/vbyc-logo.png" alt="VBYC logo" class="main-logo main-logo-normal img-responsive">
                     </a>
                 </div>
-                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <div class="col-xs-12 col-md-8">
-                    <nav class="navbar  text-center collapse navbar-collapse" role="navigation">
+                <div class="col-xs-3 col-sm-12 col-md-8">
+                    <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="col-xs-12 col-md-8 ">
+                    
+                    <nav class="navbar navbar-nav text-center collapseX navbar-collapse" role="navigation">
                         <ul class="navbar-list list-unstyled clearfix">
                             <li class="item">
                                 <a href="#" class="link normal">Sessions</a>
