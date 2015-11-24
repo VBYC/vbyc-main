b4636518a3a519800496462c55d3e02b0741a763
Nathan Hiemstra <nathanh@criticalmass.com>
2015-11-24 16:49:09 -0600

    Updating hero text and grid

--- wp-content/themes/vbyc-custom/_template-basic.php
+++ wp-content/themes/vbyc-custom/_template-basic.php
@@ -91,7 +91,11 @@
 
                     <!-- 2 columns -->
                     <div class="row">
-                        <h3 class="heading">Two columns</h3>
+                        <div class="col-xs-12 col-sm-6">
+                            <h3 class="heading">Two columns</h3>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-xs-12 col-sm-6">
                             <div class="">
                                 <?=$lorem?>
@@ -180,7 +184,7 @@
                  
 
                     <div class="row">
-                        <div class="col-xs-8 col-xs-offset-2">
+                        <div class="col-xs-12 col-md-8 col-md-offset-2">
                             <h3 class="heading">Callout text</h3>
                             <div class="content-callout">
                                 <p>
