0e1240c0743ebeba4150d4dccffd8af21e9b52d0
Nathan Hiemstra <nathanh@criticalmass.com>
2015-12-06 17:23:40 -0600

    Finishing timeline

--- wp-content/themes/vbyc-custom/_template-timeline.php
+++ wp-content/themes/vbyc-custom/_template-timeline.php
@@ -4,47 +4,34 @@
     $lorem2 = ' non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. A at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'; 
 
     $item_left = '
-        <div class="timeline-item">
-            <div class="row">
-                <div class="col-xs-12 text-center">
-                    <p class="time first">7:00 AM</p>
-                </div>
-            </div><!-- /.row -->
-            <div class="row">
-                <div class="col-xs-12 col-sm-5 col-lg-offset-1 col-lg-4">
-                    <div class="content">
-                        <div class="content content-primary-text">
-                            <h3 class="headline">Wake Up</h3>
-                            <div class="description">
-                                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                            </div>
+        <div class="row content-row">
+            <div class="col-xs-12 col-sm-5 col-lg-offset-1 col-lg-4">
+                <div class="content">
+                    <div class="content content-primary-text">
+                        <h3 class="headline">Event</h3>
+                        <div class="description">
+                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-5 col-sm-offset-2 ">
-                    <div class="content content-primary-img">
-                        <a href="#" class="link">
-                            <img src="images/template/timeline.jpg" class="timeline-img img-responsive" alt=" ">
-                        </a>
-                    </div>
+            </div>
+            <div class="col-xs-12 col-sm-5 col-sm-offset-2 ">
+                <div class="content content-primary-img">
+                    <a href="#" class="link">
+                        <img src="images/template/timeline.jpg" class="timeline-img img-responsive" alt=" ">
+                    </a>
                 </div>
-            </div><!-- /.row -->
-        </div><!-- /.timeline-item -->
+            </div>
+        </div><!-- /.row -->
     ';
 
     $item_right = '
-        <div class="timeline-item">
-            <div class="row group">
-                <div class="col-xs-12 text-center">
-                    <p class="time last">12:30 PM</p>
-                </div>
-            </div><!-- /.row -->
-            <div class="row">
+            <div class="row content-row">
                 <div class="col-xs-12 col-sm-5 col-sm-push-7 col-lg-4 col-lg-push-8
      ">
                     <div class="content">
                         <div class="content content-primary-text">
-                            <h3 class="headline">Lunch</h3>
+                            <h3 class="headline">Event</h3>
                             <div class="description">
                                 <p>Non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. A at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                                 <p>Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. A at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
@@ -60,9 +47,7 @@
                     </div>
                 </div>
             </div><!-- /.row -->
-        </div><!-- /.timeline-item -->
     ';
-
 ?>
     
     <? include("_template-includes/header.php"); ?> 
@@ -113,23 +98,77 @@
 
  
 
-                    <?=$item_left?>
-                    <?=$item_right?>
-                    <?=$item_left?>
-                    <?=$item_right?>
+                    <div class="timeline-item first">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">7:00 AM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_left?>
+                    </div><!-- /.timeline-item -->
 
-                 
+                    <div class="timeline-item">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">7:30 AM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_right?>
+                    </div><!-- /.timeline-item -->
 
-            
+                    <div class="timeline-item">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">8:00 AM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_left?>
+                    </div><!-- /.timeline-item -->
+
+                    <div class="timeline-item">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">9:00 AM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_right?>
+                    </div><!-- /.timeline-item -->
+
+                    <div class="timeline-item">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">12:00 PM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_left?>
+                    </div><!-- /.timeline-item -->
+
+                    <div class="timeline-item">
+                        <div class="row">
+                            <div class="col-xs-12 text-center">
+                                <p class="time">5:30 PM</p>
+                            </div>
+                        </div><!-- /.row -->
+                        <?=$item_right?>
+                    </div><!-- /.timeline-item -->
 
 
-
-
-
-
-                    
-
-                    
+                    <div class="timeline-item last">
+                        <div class="row row-item">
+                            <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                                <p class="time">10:00 PM</p>
+                                <div class="content">
+                                    <div class="content content-primary-text">
+                                        <h3 class="headline">TAPS</h3>
+                                        <div class="description">
+                                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh..</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- /.row -->
+                        <!-- /.row -->
+                    </div><!-- /.timeline-item -->
 
 
 
