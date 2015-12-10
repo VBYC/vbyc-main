ef56f27564694cf117bd28b55ab027b8d9c35590
Nathan Hiemstra <nathanh@criticalmass.com>
2015-12-09 20:31:34 -0600

    Lining up hamburger and logo

--- _template-includes/header.php
+++ _template-includes/header.php
@@ -4,17 +4,16 @@
 
             <div class="container">
                 <div class="row">
-                    <div class="main-logo-container col-xs-9 col-sm-12 col-md-4 text-center">
+                    <div class="main-logo-container navbar-toggle-container col-xs-12 col-md-4 text-center">
                         <a href="/" title="Home">
                             <!-- TODO: Change color with CSS http://codepen.io/helmemat/pen/ndJgz -->
                             <!-- TODO: Use just 1 file for icon too -->
                             <img src="images/logos/vbyc-logo-icon<?=$logo_version?>.svg" alt="VBYC logo" class="main-logo main-logo-icon">
                             <img src="images/logos/vbyc-logo<?=$logo_version?>.svg" alt="VBYC logo" class="main-logo main-logo-normal img-responsive">
                         </a>
-                    </div>
-                    <div class="col-xs-3 col-sm-12 col-md-8">
                         <? include("_template-includes/nav-main-toggle.php"); ?> 
                     </div>
+               
                     <div class="col-xs-12 col-md-8">
                         <? include("_template-includes/nav-main.php"); ?> 
                     </div>
@@ -27,7 +26,7 @@
                 <div class="home-hero-text">
                     <div class="container home-hero-text-inner">
                         <h1>
-                            <span class="group-1 accent">Discovering</span>  
+                            <span class="group-1">Discovering</span>  
                             <span class="group-2">nature,</span> 
                             <span class="group-3">each other, </span>
                             <span class="group-4">&amp; ourselves.</span>
