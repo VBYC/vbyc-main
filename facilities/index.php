04f9f3dde68f1ff5934500eac3db8496cc90db48
Nathan Hiemstra <nathanh@criticalmass.com>
2015-12-19 11:28:17 -0600

    Facilities - initial add

--- facilities/index.php
+++ facilities/index.php
@@ -11,6 +11,182 @@
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?> 
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/header-element.php"); ?>  
 
+ <main>
+        <section class="hero">
+            <div class="hero-image-container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="hero-image">
+                            <img src="/images/heros/facilities.jpg" alt=" ">
+                        </div>
+                    </div>
+                </div><!-- /.row -->
+            </div>
+            <div class="container-fluid hero-text-container clearfix">
+                 <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="hero-text">
+                                <p class="h4 section-name text-uppercase">Camp Life</p>
+                                <h1 class="page-name">Facilities</h1>
+                            </div>
+                        </div>         
+                    </div><!-- /.row -->
+                    <div class="row">
+                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                            <div class="hero-text">
+                                <div class="description">
+                                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                                </div> 
+                            </div> 
+                        </div>         
+                    </div><!-- /.row -->
+                </div>
+            </div>
+        </section>
+        <section class="main-content">
+            <div class="container"> 
+    
+                <article class="main-article">
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/lodge.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Lodge" data-footer="Dining together, all-camp activities, visiting the library, or lounging in the couch area at free time: The lodge is the heart of camp. It’s also home to more than 50 years of souvenirs, photos, and camp’s mascot, Cleo the Moose, whom you'll have to kiss if you get caught with your elbows on the table. The lodge is heated and has air conditioning, and has an adjacent deck that faces Great Bear Lake for cool breezes, organized activities, and hanging out with friends.">    
+                                <img src="/images/pages/facilities/sm/lodge.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/waterfront.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Waterfront" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/waterfront.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/cabins.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Cabins" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/cabins.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/stage.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Stage" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/stage.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/campfire.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Campfire Bowl" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/campfire.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/rec-area.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Covered recreation Area" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/rec-area.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/bowl.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Sports Field (The Bowl)" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/bowl.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/store.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Camp Store" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/store.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/bathhouse.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Bath House" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/bathhouse.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/archery-range.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Archery Range" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/archery-range.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/rifle-range.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Rifle Range" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/rifle-range.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/volleyball-pit.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Volleyball Pit" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/volleyball-pit.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/nature-hut.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Nature Hut" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/nature-hut.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/meditation-area.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Meditation Area" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/meditation-area.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/craft-hut.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Arts and Crafts Hut" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/craft-hut.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/flag-area.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Flag Area" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/flag-area.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/observation-deck.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Observation Deck" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/observation-deck.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/infirmary.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Infirmary" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/infirmary.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    <div class="row"> 
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/upper-20.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Soccer / football field (Upper 20)" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/upper-20.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/emergency-vehicles.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Emergency Vehicles" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/emergency-vehicles.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <a href="/images/pages/facilities/lg/museum.jpg" class="gallery" data-toggle="lightbox" data-type="image" data-gallery="multiimages" data-title="Camp Museum" data-footer="Some more details about this image.">    
+                                <img src="/images/pages/facilities/sm/museum.jpg" class="img-responsive img-fluid grid-item" alt=" " >
+                            </a>
+                        </div>
+                    </div><!-- /.row -->
+
+                    
+                </article>
+
+           
+
+                
+
+
+
+
  
+            </div><!-- /.container -->
+        </section>
+    </main>
 
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?> 
