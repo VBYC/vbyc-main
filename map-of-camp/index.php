9b34b40dba709924fa8db01d72289bdc751fe9ea
Nathan Hiemstra <nathanh@criticalmass.com>
2015-12-22 12:02:21 -0600

    Adding map of camp

--- map-of-camp/index.php
+++ map-of-camp/index.php
@@ -3,7 +3,7 @@
     $title = $title_map_of_camp ;
     $page = $url_map_of_camp ;
     $section = 'camp-life';
-    $template = 'basic-with-side-nav';
+    $template = 'basic';
     $body_attributes = 'id="page-'.$page.'" class="section-'.$section.' template-'.$template.' has-sticky-main-nav has-sidenav "';
     $description = 'FPO description';
     // $javascript_insert = '';
@@ -11,6 +11,11 @@
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?> 
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/header-element.php"); ?>  
 
+<main>   
+    <img src="/images/pages/map-of-camp/map-of-camp.jpg" class="img-full-width" alt=" ">
+   
+
+</main>
  
 
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?> 
