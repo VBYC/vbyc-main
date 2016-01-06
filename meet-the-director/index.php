fd2373a2a5d7465dc7f26711a2dd877da5fdb240
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-06 07:08:47 -0600

    Making 3 column style

--- meet-the-director/index.php
+++ meet-the-director/index.php
@@ -42,33 +42,24 @@
         <section class="main-content">
             <div class="container"> 
                 <article class="main-article">
-                    
                     <div class="row">
                         <div class="col-xs-12">
                             <h3 class="heading">Dottie Myers-Hill</h3>
-                            <div>
-                                <div style="-webkit-columns:300px;  margin-bottom: 50px">
-                                    
-                                    <p>Greetings from the woods!  My name is Dottie Myers-Hill and I’m your new (but old) Camp Director at Van Buren Youth Camp.  My last summer on staff was 1993 so my life has come full circle in the last 20 years and I find myself immensely grateful for the opportunity to “play” out here again.</p>
-                                    <p>My summers on staff allowed me to play many parts.  I was a kitchen girl, arts and crafts instructor, Penelope the Librarian and many other crazy special day characters, buddy board supervisor, head counselor, assistant director and director.</p>
-                                    <p>My favorite things about camp are listening to kids share at thought, campfire songs/skits, and watching the kids who are strangers on Sunday struggling to let go of their “best friends” at Saturday morning flag.  I love the quiet of camp in the spring and fall... it is so peaceful but also helps me appreciate the crazy loudness of the summer season.  I just love camp.</p>
-                                    <p>Since my last summer on staff I have worked as a manager in the restaurant industry, married an amazing man, and given birth to the two most perfect humans I know!  All the while, maintaining a presence at camp through volunteering on staff, speaking at Leadership and serving on the VBYC Board of Directors.</p>
-                                    <p>When I left camp in 1993, my goal was to become a teacher.  Instead, I was promoted at my restaurant job and met my husband.  So, when people ask me why I never went into teaching, I have to remind myself that, although not in the way I thought I would, I teach almost every day of my life.  I learned at camp that it is important to lead by example, be respectful and empathetic and honest.  I remembered that many hands made light work and that when you work together it is much more fun.</p>
-                                    <p>I also learned that solutions to work/life problems sometimes require creative solutions.   Seeking a true commitment from a “transient” employee pool was a trademark of my leadership style.  To me it didn’t matter that the young people I hired were only going to be around for as long as they had left in their degree program in college.  They had to care as much as I did about serving our guests well or I didn’t hire them!  I fancy myself a learner in life... and the favorite thing about my job was that I learned something new every day.  I also tried to teach something new every day... even though my kids (both at work and at home) got a little frustrated with me for it at times.</p>
-                                    <p>So, I'm happy to be back in this capacity after spending my last 20 years “away”.  My kids have grown up here and my son Zac counseled/attended leadership for the first time last summer!  It is very exciting as a parent to pass the torch in this way.</p>
-                                    <p>I am honored to be the successor to the amazing Mama P and Jason Groth... and hopeful to keep such a wonderful place on track to continued program and leadership development.</p>
-                                    <p>VBYC is a legacy for all who touch it and I am so thankful for the individuals, groups and businesses who have continued to support it.   Remember, "You belong to youth camp and youth camp belongs to you".  I look forward to meeting the campers and their families and getting to know the amazing young adults, our 2013 staff, who will help to keep that legacy alive.</p>
-                                    <p>I am a happy camper!</p>
-                                    <p>In the spirit, Dottie Myers-Hill</p>
-                                 
-                                </div>
+                            <div class="columns-reflow-fixed-3 ">
+                                <p>Greetings from the woods!  My name is Dottie Myers-Hill and I’m your new (but old) Camp Director at Van Buren Youth Camp.  My last summer on staff was 1993 so my life has come full circle in the last 20 years and I find myself immensely grateful for the opportunity to “play” out here again.</p>
+                                <p>My summers on staff allowed me to play many parts.  I was a kitchen girl, arts and crafts instructor, Penelope the Librarian and many other crazy special day characters, buddy board supervisor, head counselor, assistant director and director.</p>
+                                <p>My favorite things about camp are listening to kids share at thought, campfire songs/skits, and watching the kids who are strangers on Sunday struggling to let go of their “best friends” at Saturday morning flag.  I love the quiet of camp in the spring and fall... it is so peaceful but also helps me appreciate the crazy loudness of the summer season.  I just love camp.</p>
+                                <p>Since my last summer on staff I have worked as a manager in the restaurant industry, married an amazing man, and given birth to the two most perfect humans I know!  All the while, maintaining a presence at camp through volunteering on staff, speaking at Leadership and serving on the VBYC Board of Directors.</p>
+                                <p>When I left camp in 1993, my goal was to become a teacher.  Instead, I was promoted at my restaurant job and met my husband.  So, when people ask me why I never went into teaching, I have to remind myself that, although not in the way I thought I would, I teach almost every day of my life.  I learned at camp that it is important to lead by example, be respectful and empathetic and honest.  I remembered that many hands made light work and that when you work together it is much more fun.</p>
+                                <p>I also learned that solutions to work/life problems sometimes require creative solutions.   Seeking a true commitment from a “transient” employee pool was a trademark of my leadership style.  To me it didn’t matter that the young people I hired were only going to be around for as long as they had left in their degree program in college.  They had to care as much as I did about serving our guests well or I didn’t hire them!  I fancy myself a learner in life... and the favorite thing about my job was that I learned something new every day.  I also tried to teach something new every day... even though my kids (both at work and at home) got a little frustrated with me for it at times.</p>
+                                <p>So, I'm happy to be back in this capacity after spending my last 20 years “away”.  My kids have grown up here and my son Zac counseled/attended leadership for the first time last summer!  It is very exciting as a parent to pass the torch in this way.</p>
+                                <p>I am honored to be the successor to the amazing Mama P and Jason Groth... and hopeful to keep such a wonderful place on track to continued program and leadership development.</p>
+                                <p>VBYC is a legacy for all who touch it and I am so thankful for the individuals, groups and businesses who have continued to support it.   Remember, "You belong to youth camp and youth camp belongs to you".  I look forward to meeting the campers and their families and getting to know the amazing young adults, our 2013 staff, who will help to keep that legacy alive.</p>
+                                <p>I am a happy camper!</p>
+                                <p>In the spirit, Dottie Myers-Hill</p>
                             </div>
                         </div>
                     </div><!-- /.row -->
-            
-
-
-
                 </article> 
             </div><!-- /.container -->
         </section>
