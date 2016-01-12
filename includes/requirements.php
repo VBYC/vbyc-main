ee414614bfb6025216c633a0ad5d4bf0b4e0b925
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-12 14:24:53 -0600

    Removing mystery Facilities value

--- includes/requirements.php
+++ includes/requirements.php
@@ -1,5 +1,5 @@
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/global-variables.php"); ?> 
 <? include("".$_SERVER['DOCUMENT_ROOT']."/includes/urls.php"); ?> 
 <? include("".$_SERVER["DOCUMENT_ROOT"]."/includes/functions/check_if_selected.php"); ?> 
-<?=$page_facilities?>
+
 
