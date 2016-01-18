703120549154f927dcc0ccdfc3fdc7d2d20fdfc1
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-17 21:26:05 -0600

    Adding gzip and removing line comments in sass

--- config.rb
+++ config.rb
@@ -1,2 +1,11 @@
 require 'bootstrap-sass'
 css_dir = "/css"
+
+output_style = :expanded # :expanded or :nested or :compact or :compressed
+ 
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+ 
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+ 
