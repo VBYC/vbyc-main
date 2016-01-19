aba48a5fe1546ee5dade986a4859242dc9b02f24
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-19 07:47:55 -0600

    adding description variable

--- includes/urls.php
+++ includes/urls.php
@@ -4,75 +4,99 @@
 
 $title_about = 'About';
 $url_about = 'about';
+$description_about = 'about';
 
 $title_activities = 'Activities';
 $url_activities = 'activities';
+$description_activities = 'activities';
 
 $title_alumni = 'Alumni';
 $url_alumni = 'alumni';
+$description_alumni = 'alumni';
 
 $title_alumni_login = 'Alumni Login';
 $url_alumni_login = 'https://vanburen.campintouch.com/v2/login.aspx?cx=a';
+$description_alumni_login = 'https://vanburen.campintouch.com/v2/login.aspx?cx=a';
 
 $title_blog = 'Blog';
 $url_blog = 'blog';
+$description_blog = 'blog';
 
 $title_classes = 'Classes';
 $url_classes = 'classes';
+$description_classes = 'classes';
 
 $title_contact = 'Contact';
 $url_contact = 'contact';
+$description_contact = 'contact';
 
 $title_donate = 'Donate';
 $url_donate = 'donate';
+$description_donate = 'donate';
 
 $title_donate_cta = 'Donate today';
 $url_donate_cta = 'mailto:director@vbyc.org';
+$description_donate_cta = 'mailto:director@vbyc.org';
 
 $title_employment = 'Employment';
 $url_employment = 'https://vanburen.campintouch.com/ui/forms/application/staff/App';
+$description_employment = 'https://vanburen.campintouch.com/ui/forms/application/staff/App';
 
 $title_facilities = 'Facilities';
 $url_facilities = 'facilities';
+$description_facilities = 'facilities';
 
 $title_faqs = 'FAQs';
 $url_faqs = 'faqs';
+$description_faqs = 'faqs';
 
 $title_financial_aid = 'Financial Aid';
 $url_financial_aid = 'financial-aid';
+$description_financial_aid = 'financial-aid';
 
 $title_meet_the_director = 'Meet the Director';
 $url_meet_the_director = 'meet-the-director';
+$description_meet_the_director = 'meet-the-director';
 
 $title_explore_camp = 'Explore Camp';
 $url_explore_camp = 'explore-camp';
+$description_explore_camp = 'explore-camp';
 
 $title_news = 'News';
 $url_news = 'news';
+$description_news = 'news';
 
 $title_news_amazon = 'Donate to VBYC through your Amazon purchases';
 $url_news_amazon = 'news-amazon';
+$description_news_amazon = 'news-amazon';
 
 $title_parent_resources = 'Parent Resources';
 $url_parent_resources = 'parent-resources';
+$description_parent_resources = 'parent-resources';
 
 $title_register = 'Register';
 $url_register = 'https://vanburen.campintouch.com/v2/camper/application/app0.aspx';
+$description_register = 'https://vanburen.campintouch.com/v2/camper/application/app0.aspx';
 
 $title_rent_camp = 'Rent Camp';
 $url_rent_camp = 'rent-camp';
+$description_rent_camp = 'rent-camp';
 
 $title_schedule = 'Schedule';
 $url_schedule = 'schedule';
+$description_schedule = 'schedule';
 
 $title_typical_day = 'Typical Day';
 $url_typical_day = 'typical-day';
+$description_typical_day = 'typical-day';
 
 $title_what_to_bring = 'What to Bring';
 $url_what_to_bring = 'what-to-bring';
+$description_what_to_bring = 'what-to-bring';
 
 $title_whos_in_charge = 'Who&rsquo;s in charge?';
 $url_whos_in_charge = 'whos-in-charge';
+$description_whos_in_charge = 'whos-in-charge';
 
 // Social
 $url_facebook = 'https://www.facebook.com/VanBurenYouthCamp/';
