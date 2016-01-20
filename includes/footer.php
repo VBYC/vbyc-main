c379e6d956398e0d8e988028a6c48914d87b337b
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-20 07:21:17 -0600

    Google tracking

--- includes/footer.php
+++ includes/footer.php
@@ -60,7 +60,7 @@
             });
         </script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!-- Google Analytics: Add when go live: UA-8152298-1. -->
         <script>
                 (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
                 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
