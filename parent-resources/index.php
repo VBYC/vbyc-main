52acf9981710f3c5edc064b130024e017a8fa650
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-27 07:18:23 -0600

    Parent resources copy changes and reformatting

--- parent-resources/index.php
+++ parent-resources/index.php
@@ -29,6 +29,13 @@
                             <div class="hero-text">
                                 <p class="h4 section-name text-uppercase">Sessions</p>
                                 <h1 class="page-name">Parent Resources</h1>
+                                <div class="description">
+                                    <p>
+                                        We are sensitive to the fact that each camper has different needs. Please contact us regarding allergies and dietary requirements. Pending enough time to make arrangements, we can provide alternate meal options. And while we recognize that each camper has differing levels of comfort with being away from home, we discourage calls and visits with parents, as they can increase the intensity of homesickness. Mail, however, is always encouraged and welcome!
+                                    </p>
+                                    <a href="/<?=$url_faqs?>/" class="hero-cta">Learn more on our FAQ page</a>
+                                </div>
+
                             </div>
                         </div>         
                     </div><!-- /.row -->
@@ -42,31 +49,12 @@
 
                   
 
-                     <div class="row">
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <h3 class="heading">Things to know</h3>
-                            <ul class="">
-                               <!--  <li>(TODO:) Food: Descrioption of how we handle dietary (gluten, allergies) needs, photos, sample menu</li>
-                                <li>(TODO:) Alergy handling policy, asthma handling policy</li>
-                                <li>(TODO:) List of ways to prepare and help child cope (Wite letters, send photo, stuffed animal, encouraging talks, tell them get into activites)</li> -->
-                                <li>Learn more on our <a href="/<?=$url_faqs?>/">FAQ page</a>.</li>
-                             
-                                
-                                
-
-                            </ul>
-                        </div>
-                    </div><!-- /.row -->
-
 
                     <div class="row">
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                           
-
+                        <div class="col-xs-12 col-sm-4">
                             <h3 class="heading">Pre-camp</h3>
                             <ol class="" >
                                 <li><a href="<?=$url_register?>">Register</a></li>
-                                <li>(TODO: Mention any forms?)</li>
                                 <li>Review our <a href="/<?=$url_what_to_bring?>/">What to Bring</a> list.</li>
                                 <li>For first timers, review our <a href="/<?=$url_typical_day?>/">Typical
                                     day</a> page.</li>
@@ -79,18 +67,21 @@
                                   </ul>
                                 </li>
                             </ol>
+                        </div>
 
-
+                        <div class="col-xs-12 col-sm-4">
                             <h3 class="heading">Day of camp</h3>
-                            <ol class="" >
+                            <ol class="list-steps" >
                                 <li>Get here! <a href="<?=$url_google_map?>" target="_blank">12370 4th St., Bloomingdale, MI 49026</a>.</li>
                                 <li>Arrive Sunday at 3:00 PM (unless otherwise stated).</li>
-                                <li>When you park, a staff member give your cabin number.</li>
+                                <li>When you park, a staff member will give you your cabin number.</li>
                                 <li>Drop off your stuff in your cabin and meet your counselor.</li>
                                 <li>Come to the lodge to register.</li>
                                 <li>Swim test.</li>
                             </ol>
+                        </div>
 
+                        <div class="col-xs-12 col-sm-4">
                             <h3 class="heading">When camp is over</h3>
                             <ol class="" >
                                 <li>Come on back! <a href="<?=$url_google_map?>" target="_blank">12370 4th St., Bloomingdale, MI 49026</a>.</li>
@@ -98,6 +89,7 @@
                                 <li>Join the Final Flag ceremony.</li>
                                 <li>Come to the lodge to de-register.</li>
                                 <li>Get your stuff from your cabin.</li>
+                                <li>Go home and keep in touch via our <a href="<?=$url_alumni?>">alumni page</a> and <a href="<?=$url_facebook?>">Facebook page</a> so you’re ready for next year!</li>
                             </ol>
                         </div>
                     </div><!-- /.row -->
