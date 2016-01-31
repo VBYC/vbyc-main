6d40d5fe759829ba6b5377b4cddb122178dc4b8a
Nathan Hiemstra <nathanh@criticalmass.com>
2016-01-30 18:39:20 -0600

    Updated schedule copy

--- schedule/index.php
+++ schedule/index.php
@@ -36,8 +36,9 @@
                         <div class="col-xs-12 col-md-8 col-md-offset-2">
                             <div class="hero-text">
                                 <div class="description">
-                                    <p>Our camp sessions are designed for youth in grades 1–12, and encourage both individual growth and community with others.  During a recreational camp session, qualified, certified instructors lead organized programs in swimming, canoeing, kayaking, folk dancing, drama, nature exploration, arts and crafts, archery, riflery, sports, and team building.</p>
-                                    <p>All camp sessions include campfire skits and songs, all-camp games like water wars and capture the flag, time to plan activities together, polar-bear swims, making friends, sharing, and laughing and singing our heads off.</p>
+                                    <p>Our camps are designed for youth in grades 1–12, and encourage both individual growth and community with others. During a recreational camp session, qualified, certified instructors lead organized programs in swimming, canoeing, kayaking, folk dancing, drama, nature exploration, arts and crafts, archery, riflery, sports, and team building.</p>
+                                    <p>Every Camp session includes campfire skits and songs, all-camp games like water wars and capture the flag, time to plan activities together, polar-bear swims, making friends, sharing, and laughing and singing our heads off.
+</p>
                                 </div> 
                             </div> 
                         </div>         
@@ -70,14 +71,14 @@
                                         Cost: $110
                                     </h3>
                                     <div class="description">
-                                        <p>A short session designed to ease campers into comforting with exploring outdoors and participating in the camp experience.</p>
+                                        <p>The VBYC experience begins with Little Bear. It is  a short session designed to  help campers adjust  live comfortably in a group setting, assist children with exploring outdoors and participating in the camp experience. In this three day and two night session, campers will  swim, canoe*, play games, sing, dance, participate in skits, and eat great camp food. (*Weather or wind permitted)</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Little Bear</a></p>
                                     </div>
                                 </li>
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Extended Little Bear</span> | <span class="date nowrap">July 6-9</span> | $150</h3>
                                     <div class="description">
-                                        <p>We are offering this option to parents who feel like their new camper is ready for a slightly longer session.  This makes it a four day Little Bear experience!</p>
+                                        <p>VBYC  is offering this extension to parents who feel their new camper is ready to stay for a slightly longer session. This makes it a four day Little Bear experience!</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Extended Little Bear</a></p>
                                     </div>
                                 </li>
@@ -89,14 +90,14 @@
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Great Bear</span> | <span class="date nowrap">June 26 - July 2</span> | $325</h3>
                                     <div class="description">
-                                        <p>Improve upon camp skills, nurture leadership skills, and build community.</p>
+                                        <p>Great Bear Is for the child ready to try new experiences, Improve upon camp skills, nurture leadership skills, and build community. VBYC’s Early Youth camps do just that— they help a child grow. Campers will attend activities listed on the above first paragraph. Campers leadership and community building are fostered when campers help plan campfires, daily flag ceremonies, and participate in creative activities. Independence, self confidence, and friendships are built throughout the session but especially during free time and choice periods. All activities happen under the supervision of certified staff and trained counselors who understand how important it is to make new friends and have FUN!</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Great Bear</a></p>
                                     </div>
                                 </li>
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Explorer</span> | <span class="date nowrap">August 10-14</span> | $295</h3>
                                     <div class="description">
-                                        <p>Improve upon camp skills, nurture leadership skills, and build community, with a specific focus on nature-based activities.</p>
+                                        <p>The Explorer sessions is for children who want new experiences, learn or Improve upon camp skills, nurture leadership skills, and build community, with a specific focus on nature-based activities in all activities. Campers will attend structured activities listed at the top of the page. Leadership and community building are also fostered when campers help plan campfires, daily flag ceremonies, and participate in special activities. Independence, self confidence, and friendships are built while campers engage in free time, choice activities and gather for all-camp activities! The VBYC staff and counselors are here to help your children build friendships, nurture leadership skills build community and have a fun-filled week.</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Explorer</a></p>
                                     </div>
                                 </li>
@@ -115,7 +116,7 @@
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Frontier</span> | <span class="date nowrap">July 24-30</span>    | $325</h3>
                                     <div class="description">
-                                        <p>A week of traditional camp program, with the option of branching out into outdoor living skills for the more advanced camper. (Discovery and Frontier are sister sessions.)</p>
+                                        <p>The Frontier sessions is a week of traditional camp program with the option of branching out into outdoor living skills for the more advanced camper. Campers will have the opportunity to explore and learn more outdoor living skills in a safe environment supervised by staff at all times.Frontier campers Campers will engage in all structured classes offered during all youth camping sessions. Discovery and Frontier are sister sessions</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Frontier</a></p>
                                     </div>
                                 </li>
@@ -127,7 +128,7 @@
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Wolverine</span> | <span class="date nowrap">July 10-16</span> | $325</h3>
                                     <div class="description">
-                                        <p>In Middle Youth Camps, campers take an active role in running the program. At White Pine, we help campers identify and improve emerging leadership skills. At Wolverine, campers spend a day in a specialty program of their choice with other kids with the same interests. Both camps give campers more time to widen their circle of lifelong camp friends.</p>
+                                        <p>In Middle Youth Camps, campers take an active role in running the program. At White Pine, we help campers identify and improve emerging leadership skills. During Wolverine, campers spend a day in a specialty program of their choice with kids having  the same interests. Both camps give campers more time to widen their circle of lifelong Camp friends.</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register for Wolverine</a></p>
                                     </div>
                                 </li>
@@ -151,9 +152,9 @@
                                 <li class="item">                                    
                                     <h3 class="heading"><span class="nowrap">Leadership and Counselor Workshop </span> <br>  <span class="date nowrap">June 19-25</span> | $325</h3>
                                     <div class="description">
-                                        <p>High school students learn and develop specific leadership skills that will help them in school, clubs, communities, and relationships. They attend daily workshops, meet visiting speakers, work on special interest projects, and receive camp counselor training.</p>
-                                        <p>*A 14-year-old entering ninth grade may apply with two letters of reference and a personal essay stating why they would like to attend Leadership Workshop</p>
-                                        <p>Those interested in taking leadership to the next level can apply to participate in our counselor or staff-in-training programs.</p>
+                                        <p>High school students learn and develop specific leadership skills that will help them in school, clubs, communities, and relationships throughout their lives. They attend daily workshops, meet &amp; learn from visiting speakers, work on special interest projects, and receive camp counselor training.</p>
+                                        <p>*A 14-year-old entering ninth grade may apply to attend LW with two letters of reference and a personal essay stating why they would like to attend Leadership Workshop.</p>
+                                        <p>Those interested in taking leadership to the next level may apply to participate in our counselor or staff-in-training programs offered during most camping sessions.</p>
                                         <p><a href="<?=$url_register?>" target="_blank">Register Leadership and Counselor Workshop</a></p>
                                     </div>
                                 </li>
