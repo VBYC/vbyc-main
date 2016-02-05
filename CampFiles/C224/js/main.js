5a1e1b55b7730853de4c00e2a6d5df74b05223b9
Nathan Hiemstra <nathanh@criticalmass.com>
2016-02-05 06:44:04 -0600

    Making JS compatible with camp minder

--- CampFiles/C224/js/main.js
+++ CampFiles/C224/js/main.js
@@ -1,128 +1,68 @@
-(function( vbyc, $, undefined ) { 
+(function( vbyc, jQuery, undefined ) { 
 
-	var $body   				= $(document.body),
-		subNavSelector 			= '.navbar-list-sub',
+	var subNavSelector 			= '.navbar-list-sub',
 	    navbarSiblingsSelector  = '.navbar-list .link',
-	    sideNav 				= $('#sidenav'),
-	    headerRow 		 		= $('.header-main'),
-	    hasSidenav 				,
-	    highResPath				,
+	    headerRow 		 		= jQuery('.header-main'),
 	    containerClassName		= '.template-home .hero-animation',
-	    container 				= $(containerClassName),
-	    mainContent 			= $('.main-content'),
-	    allButFirstImage        = $('img:not(:first-child)',container),
-	    allButFirstAndLastImage = $('img:not(:first-child):not(:last-child)',container);
-	    // speedFade 				= 800,
-	    // speedInterval 			= 0;
+	    container 				= jQuery(containerClassName),
+	    mainContent 			= jQuery('.main-content')
+
 
 	vbyc.util = {
 		init: function(customValues) {
-			// If there are custom values passed in, do stuff with them
-			if (customValues) {
-				vbyc.util.initCustomValues(customValues);
-			}
-			
+	
 			// Set up main nav
 			vbyc.util.initNavbar(subNavSelector,navbarSiblingsSelector);
 			vbyc.util.initNavbarToggle(subNavSelector,navbarSiblingsSelector);
 			vbyc.util.initNavbarArrowColor(subNavSelector);
-			// vbyc.util.initNavbarSticky();
-
-			// // Set up side nav with anchor links
-			// if (hasSidenav) {
-			// 	vbyc.util.sidebarScrollSpy();
-			// 	// vbyc.util.sidebarScrollSpyScrollTo();
-			// }
-
-			// // Set up hash scrolling
-			// vbyc.util.scrollToHashOnClick();
-	  //       vbyc.util.scrollToHashOnPageLoad();
-	  //       vbyc.util.scrollToLink();
-
-			// Set up lightbox (grid)
-			// vbyc.util.initLightbox(subNavSelector,navbarSiblingsSelector);
-
-			// // News feed
-	  //       vbyc.util.newsFeed();  
-
-	  //       // Load these images after the rest of the page loads
-	  //       vbyc.util.deferImageLoad();
-	  //       if (highResPath) {
-			// 	vbyc.util.lowResPlaceholderBgImg(highResPath);
-			// }
-
-					// Set Home hero animation
-			// vbyc.util.homeHeroAnimation();
-
-			// vbyc.util.resetImages();
-	  //       vbyc.util.nextImage(); 
+			
 	        
 		},
-		initCustomValues: function(customValues) {
-			// Make this variable global inside this function
-			if (customValues.hasSidenav) {
-				hasSidenav = customValues.hasSidenav ;
-			}
-			if (customValues.highResPath) {
-				highResPath = customValues.highResPath ;
-			}
-		},
-		lowResPlaceholderBgImg: function(highResPath) {
-
-			// Once hi res image is loaded, replace low res with high res
-			// This download is started earlier in the CSS
-			var image = new Image();
-			image.onload = function () {
-			   $('.low-res-placeholder').removeClass('low-res-placeholder');
-			}
-			image.onerror = function () {
-			   $('.low-res-placeholder').removeClass('low-res-placeholder');
-			}
-			image.src = highResPath;
-		},
+	
+		
 		initNavbarToggle: function(subNavSelector,navbarSiblingsSelector) {
 
 			// Click the hamburger and expand the main nav
-			$( ".navbar-toggle" ).click( "click", function() {
-				console.log('hello');
-				$(navbarSiblingsSelector + '.expanded' ).removeClass('expanded');
-				$(subNavSelector + '.visible-xs-block' ).removeClass('visible-xs-block');
+			jQuery( ".navbar-toggle" ).click( "click", function() {
+				
+				jQuery(navbarSiblingsSelector + '.expanded' ).removeClass('expanded');
+				jQuery(subNavSelector + '.visible-xs-block' ).removeClass('visible-xs-block');
 			});
 		},
 		initNavbar: function(subNavSelector,navbarSiblingsSelector) {
 			// Main nav
-			$( ".navbar-list .link" ).on( "click", function() {
-				var navbarToggle = $('.navbar-toggle');
-				var parent = $(this).parents('.item');
-				var target = $(subNavSelector,parent);
+			jQuery( ".navbar-list .link" ).on( "click", function() {
+				var navbarToggle = jQuery('.navbar-toggle');
+				var parent = jQuery(this).parents('.item');
+				var target = jQuery(subNavSelector,parent);
 				
 				// See if nav is vertical, in SM view
-				if ($(navbarToggle).is(':visible')) {
-					if ( $(navbarSiblingsSelector + '.expanded' ).length == 0) {
+				if (jQuery(navbarToggle).is(':visible')) {
+					if ( jQuery(navbarSiblingsSelector + '.expanded' ).length == 0) {
 						// If none are already expanded, select the one just clicked
-						$(this).addClass('expanded');
-						$(target).slideDown( "fast", function() {
-							$(target).addClass('visible-xs-block').css('display','');
+						jQuery(this).addClass('expanded');
+						jQuery(target).slideDown( "fast", function() {
+							jQuery(target).addClass('visible-xs-block').css('display','');
 							// Remove 'display' attribute so this isn't visible if the screen gets wider
 						});
 					} else {
-						if ( $(this).hasClass('expanded')/* clicked = is */  ) {
+						if ( jQuery(this).hasClass('expanded')/* clicked = is */  ) {
 							// If the one just clicked is expanded, deselect it
 							// clicked = deselect
-							$(this).removeClass('expanded');
-							$(target).slideUp( "fast", function() {
-								$(target).removeClass('visible-xs-block').css('display','');
+							jQuery(this).removeClass('expanded');
+							jQuery(target).slideUp( "fast", function() {
+								jQuery(target).removeClass('visible-xs-block').css('display','');
 							});
 						} else  {
 							// If the one just clicked is NOT the one expanded, find the expanded one and deselect it. 
-							$(navbarSiblingsSelector + '.expanded' ).removeClass('expanded');
-							$(subNavSelector + '.visible-xs-block' ).slideUp( 200, function() {
-								$(subNavSelector + '.visible-xs-block' ).removeClass('visible-xs-block');
+							jQuery(navbarSiblingsSelector + '.expanded' ).removeClass('expanded');
+							jQuery(subNavSelector + '.visible-xs-block' ).slideUp( 200, function() {
+								jQuery(subNavSelector + '.visible-xs-block' ).removeClass('visible-xs-block');
 							});
 							// Select the one just clicked. 
-							$(this).addClass('expanded');
-							$(target).slideDown( 200, function() {
-								$(target).addClass('visible-xs-block').css('display','');
+							jQuery(this).addClass('expanded');
+							jQuery(target).slideDown( 200, function() {
+								jQuery(target).addClass('visible-xs-block').css('display','');
 							});
 						}
 					}
@@ -133,180 +73,15 @@
 			// If the furst item is hovered, make the arrow be the same color as the hovered item that will be right next to it
 			var target = '.link-sub:first';
 
-			$(target,subNavSelector)
+			jQuery(target,subNavSelector)
 				.on( "mouseenter", function() {
-					$(this).parents(subNavSelector).addClass('alt-arrow-emphasize');
+					jQuery(this).parents(subNavSelector).addClass('alt-arrow-emphasize');
 				})
 				.on( "mouseleave", function() {
-					$(this).parents(subNavSelector).removeClass('alt-arrow-emphasize');
+					jQuery(this).parents(subNavSelector).removeClass('alt-arrow-emphasize');
 			});
-		},
-		initNavbarSticky: function() {
-			$(headerRow).affix({
-			  offset: {
-			    // top: 120
-			  }
-			});
-		},
-		initLightbox: function() {
-			// Lightbox for Bootstrap 3 - http://ashleydw.github.io/lightbox/
- 			$('.gallery').click(function (e) {
-                e.preventDefault();
-                $(this).ekkoLightbox();
-            });
-		},
-		homeHeroAnimation: function() {
-			var target = $('.template-home .animation-outer');
-		},
-		resetImages: function() {
-	        allButFirstImage.hide();
-	    },
-	    nextImage: function() {
-	        if ($('img:last-child',container).is(':hidden')) {
-	          	vbyc.util.fadeInNextImage();
-	        } else {
-	          	allButFirstAndLastImage.hide();
-	          	vbyc.util.fadeOutLastImage();
-	        }
-	        $( "img",container).promise().done(function() {
-	        	setTimeout(vbyc.util.nextImage, speedInterval);
-	        });
-	    },
-	    fadeInNextImage: function() {
-	        $( "img:hidden:first",container).fadeIn(speedFade);
-	    },
-	    fadeOutLastImage: function() {
-	        $( "img:last",container).fadeOut(speedFade);
-	    },
-	    newsFeed: function() {
-	        var parent = $('.news-feed');
-	        var target = $('.news-feed-list');
-	        var timer;
-
-			function startTimer() {
-			    timer = setInterval(function () {
-			        $('li:first',target).slideUp( function () { 
-	                    $(this).appendTo($(target)).slideDown(); 
-	                });
-			    }, 4000);
-			}
-
-			// Start or stop animation if hover over link
-			$(parent).hover(function (event) {
-			    clearInterval(timer);
-			}, function (event) {
-			    startTimer();
-			});
-
-			startTimer();
-	    },
-		sidebarScrollSpy: function() {
-			var sidenavContainer 			= '#sidenav-container';
-			var sidenavDistanceOffset 		= $(sidenavContainer).offset();
-			var sidenavDistanceOffsetTop 	= sidenavDistanceOffset.top;
-			var sidenavPaddingTop 			= 40;
-			var headerRowHeight 			= headerRow.outerHeight(true);
-			var triggerActivateLinks 		= headerRowHeight + sidenavPaddingTop;
-			var triggerSticky 				= sidenavDistanceOffsetTop - triggerActivateLinks;
-			
-			// What ID is the the side nav? 
-			$(sideNav).affix({
-				offset: {
-					// How far you scroll down before the nav becomes sticky
-					top: triggerSticky,
-
-					// Don't let the nav scroll over top of the footer
-					bottom: function () {
-				      return (this.bottom = $('.footer').outerHeight(true))
-				    }
-				}
-			});
-
-			// Assign this scroll spy to the body 
-			$body.scrollspy({
-
-				// What ID is the immediate parent of the side nav? 
-				target: sidenavContainer,
-
-				// How far from the top that triggers the left nav items become active
-				offset: triggerActivateLinks,
-			});
-
-		},
-		scrollToHash: function(target) {
-			var sidenavContainer 			= '#sidenav-container';
-			var sidenavPaddingTop 			= 0;
-			var headerRowHeight 			= headerRow.outerHeight(true);
-			var headerRowHeightOffset       = headerRowHeight - sidenavPaddingTop;
-            var $target = $(target);
-            var scrollToCoordinate = ($target.offset().top - headerRowHeightOffset);
-
-            $('html, body').stop().animate({
-                'scrollTop': scrollToCoordinate
-            }, 500, 'swing', function () {
-
-            	// Update URL to have new hash
-            	if (history.pushState) {
-            		// This prevents Firefox from re-scrolling to the hash
-				    history.pushState(null, null, target);
-				}
-				else {
-					// Old browsers
-				    location.hash = target;
-				}
-            });
-		},
-		scrollToHashOnPageLoad: function() {
-			$(window).load(function(){
-				// Remove the # from the hash, as different browsers may or may not include it
-				var target = location.hash.replace('#','');
-				if(target != ''){
-				   target = '#' + target;
-				   vbyc.util.scrollToHash(target);
-			    }
-			});
-		},
-		scrollToHashOnClick: function() {
-			// Set up scroll
-	        $('a[href^="#"]').on('click',function (event) {
-	        	event.preventDefault();
-	        	var target = this.hash;
-	        	vbyc.util.scrollToHash(target);
-	        });
-		},
-		scrollToLink: function() {
- 
-			// Set up scroll
-	        $('a[href^="#"].scroll-to').on('click',function (e) {
-	            e.preventDefault();
-	            var target = this.hash;
-	            var $target = $(target);
-	            var scrollToCoordinate = $target.offset().top;
- 
-	            $('html, body').stop().animate({
-	                'scrollTop': scrollToCoordinate
-	            }, 500, 'swing', function () {
-	                // Update URL to have new hash
-	            	if (history.pushState) {
-	            		// This prevents Firefox from re-scrolling to the hash
-					    history.pushState(null, null, target);
-					}
-					else {
-						// Old browsers
-					    location.hash = target;
-					}
-	            });
-	        });
-	    },
-		
-		deferImageLoad: function() {
-			var imgDefer = document.getElementsByTagName('img');
-			for (var i=0; i<imgDefer.length; i++) {
-				if(imgDefer[i].getAttribute('data-src')) {
-					imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-				} 
-			} 
 		}
+		
 	}
 }( window.vbyc = window.vbyc || {}, jQuery ));
 
