fdc5d5e49ea6251c404379987f537b8c52df64a6
Nathan Hiemstra <nathanh@criticalmass.com>
2016-02-14 20:21:35 -0600

    Putting modernizer in footer

--- includes/header.php
+++ includes/header.php
@@ -39,7 +39,6 @@
 
             <link rel="apple-touch-icon" href="/apple-touch-icon.png">
             <link rel="stylesheet" href="/css/main.css">
-            <script src="/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
 
     <body <?=$body_attributes?>>
