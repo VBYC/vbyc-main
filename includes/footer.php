5595138e2063e9488940eae8c0b5f1a2a4cbb275
Nathan Hiemstra <nathanh@criticalmass.com>
2016-02-16 07:18:31 -0600

    Removing comment

--- includes/footer.php
+++ includes/footer.php
@@ -54,8 +54,6 @@
                 <?=$javascript_insert?>
             });
         </script>
-
-        <!-- Google Analytics: Add when go live: UA-8152298-1. -->
         <script>
                 (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
                 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
