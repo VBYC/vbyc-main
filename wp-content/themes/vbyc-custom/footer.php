108ed48c1333da40e3b9958c7b4cf1b8be49b487
Nathan Hiemstra <nathanh@criticalmass.com>
2016-02-29 07:41:42 -0600

    Customizing VBYC WP theme

--- wp-content/themes/vbyc-custom/footer.php
+++ wp-content/themes/vbyc-custom/footer.php
@@ -12,19 +12,72 @@
 
 	</div><!-- .site-content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php
-				/**
-				 * Fires before the Twenty Fifteen footer text for footer customization.
-				 *
-				 * @since Twenty Fifteen 1.0
-				 */
-				do_action( 'twentyfifteen_credits' );
-			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
-		</div><!-- .site-info -->
-	</footer><!-- .site-footer -->
+	<footer class="footer pad-top-7 pad-bot-7 v-offset-7">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-message">
+                            Being ourselves, together.
+                        </div>
+                        <div class="item pad-bot-4 " > 
+                            <?/*TODO include("nav-footer.php"); */?> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="footer-content clearfix">
+                        <address class="item">
+                            Van Buren Youth Camp<br>
+                            <a href="<?/*TODO=$url_google_map*/?>" target="_blank">
+                                12370 45th St.<br>
+                                Bloomingdale, MI 49026<br>
+                            </a>
+                        </address>
+                        <p class="item">  
+                            Call Us<br>
+                            <a href="tel:2695213855">269-521-3855</a>
+                        </p>
+                        <address class="item">
+                            Email Us<br>
+                            <a href="mailto:<?/*TODO=$url_email_main*/?>">mail@vbyc.org</a>
+                        </address>
+                        <p class="item">  
+                            Connect With Us<br>
+                            <a href="<?/*TODO=$url_facebook*/?>" target="_blank">Facebook</a><br>
+                            <a href="<?/*TODO=$url_twitter*/?>" target="_blank">Twitter</a><br>
+                            <a href="<?/*TODO=$url_instagram*/?>" target="_blank">Instagram</a> 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="/js/vendor/bootstrap.min.js"></script>
+    <script src="/js/vendor/ekko-lightbox.min.js"></script>
+    <script src="/js/main.js"></script>
+    <script>
+        $(document).ready(function() {
+            <?/*TODO=$javascript_insert*/?>
+        });
+    </script>
+    <script>
+            // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            // function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            // e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            // e.src='//www.google-analytics.com/analytics.js';
+            // r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            // ga('create','UA-8152298-1','auto');ga('send','pageview');
+    </script>
+
+
 
 </div><!-- .site -->
 
--- wp-content/themes/vbyc-custom/header.php
+++ wp-content/themes/vbyc-custom/header.php
@@ -8,20 +8,53 @@
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+?><!doctype html>
+<!--[if lt IE 7]>            <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>                 <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>                 <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-	<![endif]-->
+
+	<? /* Facebook */ ?>
+<!--     <meta property="og:url"                content="<?=$this_url?>" />
+    <meta property="og:title"              content="<?=$title?>" />
+    <meta property="og:description"        content="<?=$description?>" />
+    <meta property="og:image"              content="<?=$facebook_share_thumb_path.$page.'.jpg'?>" /> -->
+
+    <? /* Twitter */ ?>
+<!--     <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@vbyc">
+    <meta name="twitter:creator" content="@vbyc">
+    <meta name="twitter:title" content="<?=$title?>">
+    <meta name="twitter:description" content="<?=$description?>">
+    <meta name="twitter:image" content="<?=$facebook_share_thumb_path.$page.'.jpg'?>"> -->
+
+    <? /* Make connecting to these domains faster */ ?>
+    <link rel="dns-prefetch" href="//www.google-analytics.com">
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link rel="dns-prefetch" href="//ajax.googleapis.com">
+    <link rel="dns-prefetch" href="//connect.facebook.net">
+
+    <? /* if ($url_page == 'contact') { */ ?>
+        <link rel="dns-prefetch" href="//maps.gstatic.com">
+        <link rel="dns-prefetch" href="//maps.google.com">
+    <? /* } */  ?>
+
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]--> 
 <div id="page" class="hfeed site page-home">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
 
