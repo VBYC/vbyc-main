44391315380e3e45dc1ce6aade8d6c0557576e45
Nathan Hiemstra <nathanh@criticalmass.com>
2016-04-08 07:33:53 -0500

    Grid progress

--- wp-content/themes/vbyc-custom/templates/gallery.php
+++ wp-content/themes/vbyc-custom/templates/gallery.php
@@ -36,43 +36,40 @@ Template Name: Gallery
             <div class="container"> 
                 <article class="main-article">
                     <div class="row">
-                       
-                            <?php   
+                    <?php 
 
-                   
+                        $images = get_field('images');
+                        if( $images ): 
+                            foreach( $images as $image ): 
+                                $grid_title = $image['title'];
+                                $grid_description = $image['caption'];
+          
+  ?>
 
-                            /* MAIN CONTENT */ 
-                            include("".$_SERVER["DOCUMENT_ROOT"]."/wp-content/themes/vbyc-custom/inc/content_main.php"); 
 
-
-
-                            
-                            ?> 
-
-                            <? /*
-                            <div class="col-xs-12 col-sm-4">
-                            <? $grid_title = "Washburn Lodge"; ?>
-                            <? $grid_description = "Dining together, all-camp activities, visiting the library, or lounging in the couch area at free time makes Washburn Lodge the heart of Camp. It’s also home to more than 65 years of souvenirs, photos, and Camp’s mascot, Cleo the Moose, whom you'll have to kiss if you get caught with your elbows on the table. The lodge is both heated and air conditioned.  It has a front deck facing Great Bear Lake for people to enjoy cool breezes during activities or hanging out with friends." ?>
-                            <a href="/images/pages/facilities/lg/lodge.jpg" 
+                         
+                        <div class="col-xs-12 col-sm-4">
+                          <a href="<?php echo $image['url']; ?>" 
                             class="gallery" 
                             data-toggle="lightbox" 
                             data-type="image" 
                             data-gallery="multiimages" 
                                 data-title="<?=$grid_title?>" 
                                 data-footer="<?=$grid_description?>">
-                                <img src="/images/pages/facilities/sm/lodge.jpg" c
-                                lass="img-responsive img-fluid grid-item" alt="<?=$grid_title?>" >
+                                <img src="<?php echo $image['sizes']['thumbnail']; ?>" class="img-responsive img-fluid grid-item" alt="<?=$grid_title?>" >
                                 <h3 class="gallery-label"><?=$grid_title?></h3>
                             </a>
                             <div class="visible-xs-block details">
                                 <p class="description"><strong><?=$grid_title?></strong> &mdash; <?=$grid_description ?></p>
                             </div>
                         </div>
-                        */ ?>
+                    <?php 
+                            endforeach; 
+                        endif; 
+                    ?>
 
 
-
-                  
+                      
                     </div><!-- /.row -->
                 </article><!-- /.main-article -->
             </div><!-- /.container -->
