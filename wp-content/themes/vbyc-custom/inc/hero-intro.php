eb4ef6035b08a7323a37a65920f7372351e70f85
Nathan Hiemstra <nathanh@criticalmass.com>
2016-04-13 18:58:21 -0500

    Finishing upgrading intro link

--- wp-content/themes/vbyc-custom/inc/hero-intro.php
+++ wp-content/themes/vbyc-custom/inc/hero-intro.php
@@ -7,26 +7,24 @@
                                 <?php the_field('intro_paragraph'); ?>
 
                                 <?php 
-
-                                echo get_field("link_target" );
-                                if (!$hide_hero_intro_link) {
-
-                                    
-                                    if ( $has_link ) { 
-                                        $link_target = get_field( "intro_link_external_or_internal" );
-                                        if(  $link_target == 'external' ) { 
-                                            $this_target = ' target="_blank" ';
-                                        } ?>
-
-                                        <a  href="<?php the_field('intro_link_url'); ?>" class="hero-cta" <?php echo $this_target?>>
-                                             <?php the_field('intro_link_label'); ?>
+                                    $link_target = get_field("intro_link_target" );
+                                    if ($link_target != 'none') {
+                                        $link_label = get_field("intro_link_label");
+                                        if ($link_target == 'internal') {
+                                            $link_url = get_field("intro_link_internal");
+                                        } else if ($link_target == 'external'){
+                                            $link_url = get_field("intro_link_external");
+                                            $link_target_attribute = 'target="_blank"';
+                                        }
+                                ?>
+                                        <a  href="<?=$link_url?>" class="hero-cta" <?=$link_target_attribute?>>
+                                             <?=$link_label?>
                                         </a>
-                                    <?php 
-                                    }
-                                } ?>   
+                                <?php 
+                                 
+                                    } 
+                                ?>   
                             </div> 
                         </div> 
                     </div>         
                 </div><!-- /.row -->
-                
-            
