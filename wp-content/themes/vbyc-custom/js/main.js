f98f06a335151bab1346e69c403d4def71afb50d
Nathan Hiemstra <nathanh@criticalmass.com>
2016-04-17 11:43:55 -0500

    Fixing sub nav open/close

--- wp-content/themes/vbyc-custom/js/main.js
+++ wp-content/themes/vbyc-custom/js/main.js
@@ -101,32 +101,39 @@
 				var navbarToggle = $('.navbar-toggle');
 				var parent = $(this).parents('.menu-item');
 				var target = $(subNavSelector,parent);
+
 				
 				// See if nav is vertical, in SM view
 				if ($(navbarToggle).is(':visible')) {
+
 					if ( $(navbarSiblingsSelector + '.expanded' ).length == 0) {
 						// If none are already expanded, select the one just clicked
-						$(this).addClass('expanded');
+						$(parent).addClass('expanded');
+						// Open
 						$(target).slideDown( "fast", function() {
 							$(target).addClass('visible-xs-block').css('display','');
 							// Remove 'display' attribute so this isn't visible if the screen gets wider
 						});
 					} else {
-						if ( $(this).hasClass('expanded')/* clicked = is */  ) {
+						if ( $(parent).hasClass('expanded')/* clicked = is */  ) {
+
 							// If the one just clicked is expanded, deselect it
 							// clicked = deselect
-							$(this).removeClass('expanded');
+							$(parent).removeClass('expanded');
+							// Close
 							$(target).slideUp( "fast", function() {
 								$(target).removeClass('visible-xs-block').css('display','');
 							});
 						} else  {
 							// If the one just clicked is NOT the one expanded, find the expanded one and deselect it. 
 							$(navbarSiblingsSelector + '.expanded' ).removeClass('expanded');
+							// Close
 							$(subNavSelector + '.visible-xs-block' ).slideUp( 200, function() {
 								$(subNavSelector + '.visible-xs-block' ).removeClass('visible-xs-block');
 							});
 							// Select the one just clicked. 
-							$(this).addClass('expanded');
+							$(parent).addClass('expanded');
+							// Open
 							$(target).slideDown( 200, function() {
 								$(target).addClass('visible-xs-block').css('display','');
 							});
